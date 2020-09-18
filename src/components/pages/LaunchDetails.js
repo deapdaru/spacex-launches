0e41bb1ea303704c463c5147ca5d54c963522e72
Deap Daru <deapdaru@gmail.com>
2020-09-18 16:52:08 +0530

    Add 'mission_patch' using ternary operator
    The error was that the property links would be undefined and only
    the request would be complete would it load the objects inside so
    applying ternary operator solved the issue.

---
+++ src/components/pages/LaunchDetails.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Card, CardContent, CardHeader, CardMedia, Grid, Typography } from '@material-ui/core';
+
+class LaunchDetails extends Component {
+    render() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const launchStyle = {
+            color: this.props.launch.launch_success ? "#4caf50" : "#f44336"
+        }
+        return (
+            <Grid container>
+                <Grid item xs={1}></Grid>
+                <Grid item xs={10}>
+                    <Card style={{backgroundColor: "#ddd"}}>
+                        <CardHeader title={"Mission Name: " + this.props.launch.mission_name} subheader={new Date(this.props.launch.launch_date_unix*1000).toLocaleDateString("en-GB", options)}/>
+                        <CardMedia style={cardImg} component="img" alt={this.props.launch.mission_name} src={this.props.launch.links !== undefined ? this.props.launch.links.mission_patch: ""} title={this.props.launch.mission_name} />
+                        <CardContent>
+                            <Typography variant="body1">{"Name of the rocket launched was " + (this.props.launch.rocket !== undefined ? this.props.launch.rocket.rocket_name : "") + "."}</Typography>
+                            <Typography variant="body1">{"Name of the launch site was " + (this.props.launch.launch_site !== undefined ? (this.props.launch.launch_site.site_name_long + " (" + this.props.launch.launch_site.site_name + ")") : "") + "."}</Typography>
+                            <Typography variant="body2" style={launchStyle}>{"Launch Status: " + (this.props.launch.launch_success ? "Success!" : "Failure!")}</Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={1}></Grid>
+            </Grid>
+        )
+    }
+}
+
+const cardImg = {
+    maxWidth: "500px",
+    margin: "0 auto",
+    padding: "20px 0"
+}
+
+export default LaunchDetails;
